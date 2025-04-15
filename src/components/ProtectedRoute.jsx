0123c97cc0321662bb7d6e8387046c81b4df297a
emzzy <ezehemmanuel265@gmail.com>
2025-04-15 18:51:22 +0100

    Protected routes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react'
 
 
 function ProtectedRoute({children}) {
-    const [isAuthorized, setIsAuthorized] = useState()
+    const [isAuthorized, setIsAuthorized] = useState(null);
 
     useEffect(() => {
         auth().catch(() => setIsAuthorized(false))
@@ -14,9 +14,10 @@ function ProtectedRoute({children}) {
 
     const refreshToken = async () => {
         const refreshToken = localStorage.getItem(REFRESH_TOKEN)
-        try{
-            const res = await api.post('/api/token/refresh', {
-                refresh: refreshToken
+
+        try {
+            const res = await api.post('/api/token/refresh/', {
+                refresh: refreshToken,
             });
             if (res.status === 200){
                 localStorage.setItem(ACCESS_TOKEN, res.data.access)
@@ -28,11 +29,10 @@ function ProtectedRoute({children}) {
             console.log(error);
             setIsAuthorized(false);
         }
-
     }
-
     const auth = async () => {
         const token = localStorage.getItem(ACCESS_TOKEN);
+        
         if (!token){
             setIsAuthorized(false);
             return;
@@ -47,7 +47,7 @@ function ProtectedRoute({children}) {
             setIsAuthorized(true);
         }
     }
-
+    
     if (isAuthorized === null) {
         return <div>Loading....</div>
     }
