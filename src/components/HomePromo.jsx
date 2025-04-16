cc9059a178c7d32ee7dac01b85368a0be667bf59
emzzy <ezehemmanuel265@gmail.com>
2025-04-16 23:42:39 +0100

    Home page component

---
+++ src/components/HomePromo.jsx
@@ -0,0 +1,24 @@
+import "../styles/HomePromo.css"
+
+function HomePromo () {
+    return (
+        <div className="promo-container">
+            <div className="promo-text">
+                <h1>Connecting You to Expert Care in Just a Few Clicks</h1>
+                <p>Get personalised care with just a tap.</p>
+                <p>Trusted professionals at your fingertips.</p>
+                <p>Your path to better health starts here</p>
+                <button className="promo-button">
+                    Book Appointment
+                </button>
+            </div>
+            <div className="promo-image">
+                <img 
+                    src="" 
+                    alt="" 
+                />
+            </div>
+        </div>
+    )
+}
+export default HomePromo
