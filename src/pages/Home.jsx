a741db8c0ff6f12d013f8855dd6202f4916440f4
emzzy <ezehemmanuel265@gmail.com>
2025-04-18 23:45:19 +0100

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
 import React from "react"
 import HomePromo from "../components/HomePromo";
+import Services from "./Services";
+import api from "../api";
+
 
 function Home() {
+    const [doctors, setDoctors] = useState([]);
+    const [] = useState();
+
     return (
         <div>
             <HomePromo />
+            <Services />
+            
         </div>
     );
 };
