835a5cb59d0d8c60fe729f0fb9d864b712400b77
emzzy <ezehemmanuel265@gmail.com>
2025-04-18 23:44:40 +0100

    Home page view

--- src/components/HomePromo.jsx
+++ src/components/HomePromo.jsx
@@ -1,24 +1,53 @@
-import "../styles/HomePromo.css"
+import "../styles/HomePromo.css";
+import heartImage from '../assets/images/heart-image.png';
+import happyDoctor from '../assets/images/happyDoctor.jpg';
 
 function HomePromo () {
     return (
-        <div className="promo-container">
-            <div className="promo-text">
-                <h1>Connecting You to Expert Care in Just a Few Clicks</h1>
-                <p>Get personalised care with just a tap.</p>
-                <p>Trusted professionals at your fingertips.</p>
-                <p>Your path to better health starts here</p>
-                <button className="promo-button">
-                    Book Appointment
-                </button>
+        <>
+            <div className="promo-container">
+                <div className="promo-text">
+                    <h1>Connecting You to <br /> Expert Care in Just a <br /> Few Clicks!</h1>
+                    <p>Get personalised care with just a tap. <br />
+                    Trusted professionals at your fingertips. <br />
+                    Your path to better health starts here</p>
+                    <button className="promo-button">
+                        Book Appointment
+                    </button>
+                </div>
+                <div className="promo-image">
+                    <img 
+                        src={heartImage} 
+                        alt="heart-image" 
+                    />
+                </div>
             </div>
-            <div className="promo-image">
-                <img 
-                    src="" 
-                    alt="" 
-                />
+            
+            <div className="health-detail">
+                <div className="doctor-image">
+                    <img
+                        src={happyDoctor}
+                        alt="happyDoctor"
+                        className="rounded-lg w-full h-auto object-cover"
+                    />
+                </div>
+                <div className="text-section">
+                    <div className="header">
+                        <h1>Welcome to our Healthcare Platform</h1>
+                        <p>Connect with certified professionals instantly</p>
+                    </div>
+                    <div className="features">
+                        <h2>Our Services</h2>
+                        <ul className="features-list">
+                            <li>Instant access to certified professionals</li>
+                            <li>Book appointments, chat and video call without the hassle</li>
+                            <li>Secure, private, and always available when you need it.</li>
+                            <li>Your path to better health starts here</li>
+                        </ul> 
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 export default HomePromo
