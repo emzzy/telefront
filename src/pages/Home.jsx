796c448cb3a3ce92a9e91ff50c38d27d51b9f657
emzzy <ezehemmanuel265@gmail.com>
2025-04-19 23:49:51 +0100

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,27 @@ import React from "react"
 import HomePromo from "../components/HomePromo";
 import Services from "./Services";
 import api from "../api";
+import { data } from "react-router-dom";
 
 
 function Home() {
     const [doctors, setDoctors] = useState([]);
     const [] = useState();
 
+    useEffect(() => {
+        getDoctors();
+    })
+
+    const getDoctors = () => {
+        api.get('api/users/medical-professionals/')
+        .then((res) => res.data)
+        .then((data) => setDoctors(data)).catch(error);
+    };
+
     return (
         <div>
             <HomePromo />
             <Services />
-            
         </div>
     );
 };
