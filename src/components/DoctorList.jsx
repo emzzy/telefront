26e578f2b1308572bc75a921125bc74039b77a5f
emzzy <ezehemmanuel265@gmail.com>
2025-04-22 20:51:16 +0100

    Display doctors in homepage

---
+++ src/components/DoctorList.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import api from '../api';
+import { TfiLocationPin } from "react-icons/tfi";
+import '../styles/DoctorList.css'
+
+const DoctorList = () => {
+    const [doctors, setDoctors] = useState([]);
+    
+    useEffect(() => {
+        getDoctors();
+    }, [])
+
+    const getDoctors = () => {
+        api
+            .get('api/users/all-doctors/')
+            .then((res) => res.data)
+            .then((data) => {
+                setDoctors(data); 
+                console.log(data) 
+            })
+            .catch((err) => alert(err));
+    };
+
+    return (
+        <>
+            <div className="doctors-container">
+                <h1>Our Doctors</h1>
+                <div className="doctors-list">
+                    {doctors.map((doctors, index) => (
+                        <div
+                            className="doctor-card"
+                            key={index}
+                            onClick={() => handleClick(doctors.link)}
+                            role='button'
+                            style={{ cursor: "pointer" }}
+                        >
+                            <div className="icon"> {doctors.image} </div>
+                            <h3> {doctors.title} {doctors.first_name} {doctors.last_name} </h3>
+                            <h4> <TfiLocationPin />  {doctors.location} </h4>
+                            <button className="doctor-button">
+                                Chat
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DoctorList
