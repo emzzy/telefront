f01fe2c98156e0280bcacd0e30824207dc210eeb
emzzy <ezehemmanuel265@gmail.com>
2025-04-26 14:35:56 +0100

    Doctor details

---
+++ src/components/DoctorDetails.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+
+const DoctorDetails = () => {
+    const location = useLocation();
+    const { doctor } = location.state || {};
+    return (
+        <div>
+            <h1>Doctor details</h1>
+            {doctor ? (
+                <>
+                    <h2>
+                        {doctor.title} {doctor.first_name} {doctor.last_name}
+                    </h2>
+                    <p>Location: {doctor.location}</p>
+                    <div>
+                        {doctor.image}
+                    </div>
+                </>
+            ) : (
+                <p>No doctor data found!</p>
+            )}
+        </div>
+    )
+}
+
+export default DoctorDetails
