392e2ed1efa9913e4eb6c681734bd336ae309bc7
emzzy <ezehemmanuel265@gmail.com>
2025-04-28 22:30:31 +0100

    Update

--- src/components/DoctorList.jsx
+++ src/components/DoctorList.jsx
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react'
 import api from '../api';
 import { TfiLocationPin } from "react-icons/tfi";
 import '../styles/DoctorList.css'
+import { useNavigate } from 'react-router-dom';
+
 
 const DoctorList = () => {
     const [doctors, setDoctors] = useState([]);
+    const navigate = useNavigate();
+
+    const handleClick = (link, doctor) => {
+        navigate(link, { state: { doctor } })
+    };
     
     useEffect(() => {
         getDoctors();
@@ -30,13 +37,17 @@ const DoctorList = () => {
                         <div
                             className="doctor-card"
                             key={index}
-                            onClick={() => handleClick(doctors.link)}
+                            onClick={() => handleClick(`/doctor/${doctors.id}`, doctors)}
                             role='button'
                             style={{ cursor: "pointer" }}
                         >
                             <div className="icon"> {doctors.image} </div>
-                            <h3> {doctors.title} {doctors.first_name} {doctors.last_name} </h3>
-                            <h4> <TfiLocationPin />  {doctors.location} </h4>
+                            <h3> 
+                                {doctors.title} {doctors.first_name} {doctors.last_name} 
+                            </h3>
+                            <h4> 
+                                <TfiLocationPin />  {doctors.location}
+                            </h4>
                             <button className="doctor-button">
                                 Chat
                             </button>
