c514f48fec8889ec714a98e3b310c826702a8401
emzzy <ezehemmanuel265@gmail.com>
2025-05-09 22:02:43 +0100

    List service details and Doctor details

--- src/pages/ServiceDetail.jsx
+++ src/pages/ServiceDetail.jsx
@@ -15,10 +15,9 @@ const ServiceDetail = () => {
 
     if (!service) return <p>Loading..............</p>
     console.log(`The service detail is ${id}`)
-
     return (
-        <div className='service-detail'>
-            <h3>Service Detail</h3>
+        <div className='max-w-5xl mx-auto py-10'>
+            <h3 className='text-3xl font-bold text-blue-900 mb-2'>Service Detail</h3>
             <div className='service-name'>
                 <h1> {service.id}: {service.name} </h1>
                 <p> {service.description} </p>
@@ -29,40 +28,47 @@ const ServiceDetail = () => {
                     alt={service.name}
                 />
             </div>
-            <h2>Available Doctors</h2>
-            {service.available_doctors.map((doctor) => (
-                doctor.medicalprofessional ? (
-                    <div
-                        key={doctor.id}
-                        className='flex items-center justify-between bg-white shadow-md rounded-lg p-6'
-                    >
-                        <div className='flex items-center space-x-4'>
-                            <img
-                                src={`http://localhost:8000${doctor.medicalprofessional.image}`}
-                                alt="doctorImage"
-                                className='w-20 h-20 object-cover rounded-full'
-                            />
+            <h2 className='text-3xl font-bold text-blue-900 mb-2'>
+                Available Doctors
+            </h2>
+            <p className='text-gray-600 mb-8'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, reiciendis.
+            </p>
+            <div className='space-y-6'>
+                {service.available_doctors.map((doctor) => (
+                    doctor.medicalprofessional ? (
+                        <div
+                            key={doctor.id}
+                            className='flex items-center justify-between bg-white shadow-md rounded-lg p-6'
+                        >
+                            <div className='flex items-center space-x-4'>
+                                <img
+                                    src={`http://localhost:8000${doctor.medicalprofessional.image}`}
+                                    alt="doctorImage"
+                                    className='w-20 h-20 object-cover rounded-full'
+                                />
+                            </div>
+                            <h3 className='text-xl font-semibold'>
+                                {doctor.first_name} {doctor.last_name}
+                            </h3>
+                            <p className='text-sm text-gray-700'>
+                                <span className='font-medium'> Time Available: </span>{' '}
+                                {new Date(doctor.medicalprofessional.available_appointment_date).toLocaleString('en-GB', {
+                                    day: 'numeric',
+                                    month: 'long',
+                                    year: 'numeric',
+                                })}
+                            </p>
+                            <p className='text-sm text-gray-700'>
+                                <span className='font-medium'>Price: </span> ${service.cost}
+                            </p>
+                            <button className='bg-red-600 text-white px-5 py-2 rounded-md hover:bg-red-700 transition duration-300'>
+                                Book Now
+                            </button>
                         </div>
-                        <h3 className='text-xl font-semibold'>
-                            {doctor.first_name} {doctor.last_name}
-                        </h3>
-                        <p className='text-sm text-gray-700'>
-                            <span className='font-medium'> Time Available: </span>{' '}
-                            {new Date(doctor.medicalprofessional.available_appointment_date).toLocaleString('en-GB', {
-                                day: 'numeric',
-                                month: 'long',
-                                year: 'numeric',
-                            })}
-                        </p>
-                        <p className='text-sm text-gray-700'>
-                            <span className='font-medium'>Price: </span> ${service.cost}
-                        </p>
-                        <button className='bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-300'>
-                            Book Now
-                        </button>
-                    </div>
-                ) : null
-            ))}
+                    ) : null
+                ))}
+            </div>
             {/* <div className='available-doctors'>
                 <div className='service-doctor'>
                     {service.available_doctors}
