efbe31a7dfc9b845c47efd91e72537575d7984b5
emzzy <ezehemmanuel265@gmail.com>
2025-05-11 22:06:49 +0100

    Added Book Appointment page

--- src/pages/BookAppointment.jsx
+++ src/pages/BookAppointment.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
 
 const BookAppointment = () => {
+  const { serviceId, doctorId } = useParams();
+  const [service, setService] = useState();
+
+  useEffect(() => {
+    api.get(`base/service/${id}`)
+    .then((res) => setService(res.data))
+    .catch((err) => console.error(err));
+}, [id]);
+
+  const handleSubmit = async (e) => {
+    const serviceDetail = []
+  }
   return (
     <div>
       
     </div>
+
   )
 }
 
-export default BookAppointment
+export default BookAppointment;
