eb5720e8d77c9f96a58c4f84f60d9e06afd6ca22
emzzy <ezehemmanuel265@gmail.com>
2025-05-15 22:21:34 +0100

    Added Context for Services when Booking appointment

---
+++ src/context/AppointmentContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+const AppointmentContext = createContext();
+
+export const AppointmentProvider = ({ children }) => {
+    const [serviceDetails, setServiceDetails] = useState(null);
+    const [doctorDetails, setDoctorDetails] = useState(null);
+
+    return (
+    <AppointmentContext.Provider value={{
+        serviceDetails, 
+        setServiceDetails, 
+        doctorDetails, 
+        setDoctorDetails
+    }}>
+        {children}
+    </AppointmentContext.Provider>
+    );
+};
+
+export const useAppointmentContext = () => useContext(AppointmentContext);
