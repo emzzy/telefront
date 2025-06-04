4c3fe29dad3cee68e4643cc806a2a6de5323f20c
emzzy <ezehemmanuel265@gmail.com>
2025-06-04 23:48:16 +0100

    Tailwind setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AppointmentProvider } from './context/AppointmentContext.jsx'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
+import './styles/global.css'
 
 
 const clientId = import.meta.env.VITE_PAYPAL_CLIENT_ID;
