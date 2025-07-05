840cb78c363de1c5bafdaa6d4f983d4d2bfd5fa7
emzzy <ezehemmanuel265@gmail.com>
2025-07-05 23:35:29 +0100

    Updated Doctor dashboard component

---
+++ src/pages/Doctordashboard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { fetchDoctorData } from '../api/fetchData';
+import { useQuery } from '@tanstack/react-query';
+import Dashboard from '../components/Dashboard';
+import SideBar from '../components/SideBar';
+// import { data } from 'react-router-dom';
+
+
+const Doctordashboard = () => {
+    const { data, isError, isLoading } = useQuery({
+        queryKey: ['doctorDashboardData'],
+        queryFn: fetchDoctorData,
+    });
+
+    if (isLoading) return <div>Loading....</div>
+    if (isError) return <div>Error loading data</div>
+
+    return (
+        <>
+        <Dashboard
+            userType='doctor'
+            userData={data.doctor}
+            appointments={data.appointments}
+            notifications={data.notifications}
+        />
+        </>
+    );
+};
+export default Doctordashboard;
