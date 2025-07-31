4a1b09743d9cfcf962d1a6d59633e0b34650516d
emzzy <ezehemmanuel265@gmail.com>
2025-07-31 01:50:06 +0100

    HotFix

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import SideBar from '../SideBar';
+import DashboardNavbar from '../DashboardNavbar';
+
+
+const Layout = () => {
+  return (
+    <div className="app-layout">
+      <DashboardNavbar />
+      <div className="main-content">
+        <SideBar />
+        <div className="content-area">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
